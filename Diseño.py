4bd857e3100af5b4e4d0ec5f2ebb04b7c5486ecd
Marvin291-dev <marvin.velasquez@udeo.edu.gt>
2025-08-11 11:29:17 -0600

    primer if

--- Diseño.py
+++ Diseño.py
@@ -1,1 +1,18 @@
-while
+Registrar = {}
+while True:
+    print(f"Bienvenido")
+    print("1. Registrar Cinepolis")
+    print("2. Editar")
+    print("3. Eliminar")
+    print("4. Buscar")
+    print("5. Salir")
+
+    opcion = int(input("Ingrese una opción"))
+    if opcion == "1":
+        Cantidad = int(input("Ingrese la cantidad de Persona"))
+        for i in range(Cantidad):
+            print(f"\Cinepolis {i + 1}")
+            Codigo = int(input("Ingrese el codigo de la persona"))
+            Nombre = input("Ingrese el nombre")
+            Apellido = input("Ingrese el apellido")
+            Edad = int(input("Ingrese la edad de la persona 15/06/2009"))
