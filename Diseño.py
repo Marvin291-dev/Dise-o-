c2434aac0a42d09345a023e38b1685bc453f5507
Marvin291-dev <marvin.velasquez@udeo.edu.gt>
2025-08-11 11:34:40 -0600

    primer if

--- Diseño.py
+++ Diseño.py
@@ -12,7 +12,14 @@ while True:
         Cantidad = int(input("Ingrese la cantidad de Persona"))
         for i in range(Cantidad):
             print(f"\Cinepolis {i + 1}")
-            Codigo = int(input("Ingrese el codigo de la persona"))
-            Nombre = input("Ingrese el nombre")
-            Apellido = input("Ingrese el apellido")
-            Edad = int(input("Ingrese la edad de la persona 15/06/2009"))
+            Codigo = int(input("Ingrese el codigo de la persona: "))
+            Nombre = input("Ingrese el nombre: ")
+            Apellido = input("Ingrese el apellido: ")
+            Edad = int(input("Ingrese la edad de la persona 15/06/2009: " ))
+            Areas = input("Ingrese la area de la persona que desea registrar: ")
+            Registrar[Codigo] = {
+                "Nombre": Nombre,
+                "Apellido": Apellido,
+                "Edad": Edad,
+                "Areas": Areas
+            }
